18d8c58d88be7fa2edb4480adcb867549ebf8def
Dustin Davisson <dustin.davisson@gmail.com>
2022-08-29 16:22:24 -0700

    remove yargs.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const yargs = require('yargs');
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
