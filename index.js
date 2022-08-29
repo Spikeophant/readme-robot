02fa8d7c493308e8fa21d6fbf08a660f1922d581
Dustin Davisson <dustin.davisson@gmail.com>
2022-08-29 16:21:25 -0700

    Add yargs and create function to return answers.  Want to be able to retry, create, update, etc.

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const yargs = require('yargs');
 // TODO: Create an array of questions for user input
-const questions = ['Project Title?','Description of Project?', 'Installation Instructions!', 'Usage Information',
-    'Contribution Guidelines?', 'Testing Instructions?'];
-inquirer.prompt(questions)
-    .then((answers) => {
-        console.log(answers)
-        return answers;
-    })
-    .catch((err) => {
-        console.log(err);
-        console.log("It's dead Jim, there's nothing I can do.");
-        process.exit();
-    });
+const questions = [
+    {
+        type: 'input',
+        message: 'Project Title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Description of Project?',
+        name: 'desc'},
+    {
+        type: 'input',
+        message: 'Installation Instructions!',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Usage Information',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Contribution Guidelines?',
+        name: 'contrib'
+    },
+    {
+        type: 'input',
+        message: 'Testing Instructions?',
+        name: 'testing'
+    }
+];
+
+getReadmeContent = () => {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+            return answers;
+        })
+        .catch((err) => {
+            console.log(err);
+            console.log("It's dead Jim, there's nothing I can do.");
+            process.exit();
+        });
+}
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+writeToFile = (fileName, data) => {
+
+}
 
 
 
