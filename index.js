b28dd32a1c6624b6b290470c487bb86192582222
Dustin Davisson <dustin.davisson@gmail.com>
2022-08-29 17:04:01 -0700

    Start to mess with getting all data.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -33,31 +33,49 @@ const questions = [
         name: 'testing'
     }
 ];
+getFilePath = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Path to ReadMe, leave empty for ./README.md',
+            name: 'path'
+        }
+    ]).then((path) => {
+      return path;
+    })
+        .catch((err) => {
+            console.log(err);
+            console.log('Error obtaining path, please try again after some time.');
+            process.exit(127);
+        })
+}
 
 getReadmeContent = () => {
     inquirer.prompt(questions)
         .then((answers) => {
-            console.log(answers)
             return answers;
         })
         .catch((err) => {
             console.log(err);
             console.log("It's dead Jim, there's nothing I can do.");
-            process.exit();
+            process.exit(44);
         });
 }
 
 
 // TODO: Create a function to write README file
 writeToFile = (fileName, data) => {
-
+    console.log(fileName);
+    console.log(data);
 }
 
 
 
 // TODO: Create a function to initialize app
-function init() {
-
+init = () => {
+    const answers = getReadmeContent();
+    const path = getFilePath();
+    writeToFile(answers, path)
 }
 
 // Function call to initialize app
