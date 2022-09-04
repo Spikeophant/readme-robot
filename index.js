957931e1466cd6e68f2bfbe17b0a038b480a9fa1
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-03 22:06:38 -0700

    Get the data, put in template, write template to file.

--- index.js
+++ index.js
@@ -31,51 +31,102 @@ const questions = [
         type: 'input',
         message: 'Testing Instructions?',
         name: 'testing'
+    },
+    {
+        type: 'input',
+        message: 'Path to README.md',
+        name: 'path'
+    },
+    {
+        type: 'input',
+        message: 'Email address',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Github profile link?',
+        name: 'github'
     }
 ];
-getFilePath = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Path to ReadMe, leave empty for ./README.md',
-            name: 'path'
-        }
-    ]).then((path) => {
-      return path;
-    })
-        .catch((err) => {
-            console.log(err);
-            console.log('Error obtaining path, please try again after some time.');
-            process.exit(127);
-        })
-}
-
-getReadmeContent = () => {
-    inquirer.prompt(questions)
-        .then((answers) => {
-            return answers;
-        })
-        .catch((err) => {
-            console.log(err);
-            console.log("It's dead Jim, there's nothing I can do.");
-            process.exit(44);
-        });
-}
-
 
 // TODO: Create a function to write README file
-writeToFile = (fileName, data) => {
-    console.log(fileName);
-    console.log(data);
+writeToFile = (data) => {
+    console.log('Using path ' + data.path);
+    const readmeTemplateLiteral = `
+    # ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+${data.usage}
+
+## Credits
+
+${data.email}
+${data.github}
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+    `
+
+    fs.writeFile(data.path, readmeTemplateLiteral, (err) => {
+        if (err)
+            console.log(err);
+        else {
+            console.log('README written.')
+            console.log('README contents: ')
+            console.log(fs.readFileSync(data.path), 'utf8')
+        }
+    })
+
 }
 
 
 
 // TODO: Create a function to initialize app
-init = () => {
-    const answers = getReadmeContent();
-    const path = getFilePath();
-    writeToFile(answers, path)
+function init() {
+    inquirer.prompt(questions)
+      .then((answers) => {
+            writeToFile(answers);
+      })
+      .catch((err) => {
+          console.log(err);
+          console.log("It's dead Jim, there's nothing I can do.");
+          process.exit(44);
+      });
 }
 
 // Function call to initialize app
