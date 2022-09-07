311905e74a325a272f4b34836d5a4a672a8ea144
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-06 17:40:24 -0700

    Remove templating stuff from template literal.

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ ${data.desc}
 
 ${badge}
 
-## Table of Contents (Optional)
+## Table of Contents
 
 If your README is long, add a table of contents to make it easy for users to find what they need.
 
